3a981af48c253c4fd5f946061956e7ae455f15ed
Sidra Naeem <107788062+sidranaeemsidi@users.noreply.github.com>
2023-05-02 11:12:51 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+
+
+
+const toggleNavbar = () => {
+    // alert("please clicked")
+    nav_header.classList.toggle('active');
+}
+
+mobile_nav.addEventListener( 'click' , ()=> toggleNavbar());
